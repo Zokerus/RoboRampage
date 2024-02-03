ce2bdef9a72033d08e093b23dd5fc68d3eec51af
Zokerus <Zokerus@users.noreply.github.com>
2024-02-03 12:10:18 +0100

    add enemy attack animation

--- Enemy/Enemy.cs
+++ Enemy/Enemy.cs
@@ -3,51 +3,89 @@ using System;
 
 public partial class Enemy : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+	[Export]
+	public float speed = 5.0f;
+	[Export]
+	public float jumpHeight = 1.0f;
+	[Export]
+	public float aggroRange = 12.0f;
+	[Export]
+	public float attackRange = 1.5f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 	private NavigationAgent3D m_navAgent;
+	private AnimationPlayer m_animPlayer;
 	private Player m_player;
+	private bool m_provoked = false;
 
     public override void _Ready()
     {
         base._Ready();
 		m_navAgent = GetNode<NavigationAgent3D>("NavigationAgent3D");
+		m_animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 		m_player = (Player)GetTree().GetFirstNodeInGroup("player");
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+		if (m_provoked)
+		{
+			m_navAgent.TargetPosition = m_player.GlobalPosition;
+		}
     }
 
     public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
-		m_navAgent.TargetPosition = m_player.GlobalPosition;
 		Vector3 next_Position = m_navAgent.GetNextPathPosition();
 
 		// Add the gravity.
 		if (!IsOnFloor())
 			velocity.Y -= gravity * (float)delta;
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+		float distance = this.GlobalPosition.DistanceTo(m_player.GlobalPosition);
+		Vector3 direction = this.GlobalPosition.DirectionTo(next_Position);
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (distance <= aggroRange)
+		{
+			m_provoked = true;
+		}
+
+		if(m_provoked) 
+		{
+			if (distance <= attackRange)
+			{
+				m_animPlayer.Play("attack");
+			}
+		}
+
 		if (direction != Vector3.Zero)
 		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
+			LookAtTarget(direction);
+			velocity.X = direction.X * speed;
+			velocity.Z = direction.Z * speed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, speed);
 		}
 
 		Velocity = velocity;
-		//MoveAndSlide();
+		MoveAndSlide();
+	}
+
+	public void LookAtTarget(Vector3 direction)
+	{
+		Vector3 adjustedDirection = direction;
+		adjustedDirection.Y = 0;
+		this.LookAt(this.GlobalPosition + adjustedDirection, Vector3.Up, true);
+	}
+
+	public void Attack()
+	{
+
 	}
 }
